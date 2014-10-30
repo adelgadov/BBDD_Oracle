29e2ec6aad157eaf8ec23b89cec919e186ae5c11
alvaro <alvaro_delgado_1994@hotmail.com>
2014-10-30 20:21:06 +0100

    Funciona perfectamente introduciendo la sentencia SQL

---
+++ bdatos.php
@@ -0,0 +1,63 @@
+<?php
+class bdatos {
+
+	public $host;
+	public $user;
+	public $password;
+
+
+
+	public function __construct ($host, $user, $password, $busqueda) {
+		$this -> user = $user;
+		$this -> password = $password;
+		$this -> host = $host;
+
+
+
+	}
+
+	public function &conectar() {
+		$connection = oci_connect($this -> user, $this -> password, $this -> host);
+		return $connection;
+	}
+	public function consulta ($connection, $busqueda) {
+
+		$query = oci_parse($connection, $busqueda);
+		oci_execute($query);
+		return $query;
+	}
+	public function resultado ($query) {
+
+        echo "<table border='1'>\n";
+        $primera = oci_fetch_array($query, OCI_ASSOC+OCI_RETURN_NULLS);
+
+        if ($primera) {
+            $columna = array_keys($primera);
+        ?>
+            <tr class="titulo">
+        <?php
+        foreach ($columna as $campo): ?>
+            <th> <?php echo $campo; ?> </th>
+        <?php endforeach; ?>
+            </tr>
+
+            <tr>
+                <?php foreach($primera as $valor): ?>
+                    <td> <?php echo $valor; ?> </td>
+                <?php endforeach ?>
+            </tr>
+        <?php
+        }
+
+
+        while ($row = oci_fetch_array($query, OCI_ASSOC+OCI_RETURN_NULLS)) {
+            echo "<tr>\n";
+            foreach ($row as $key => $value) {
+                echo "    <td>" . ($value !== null ? htmlentities($value, ENT_QUOTES) : "") . "</td>\n";
+            }
+            echo "</tr>\n";
+        }
+        echo "</table>\n";
+
+	}
+	}
---
+++ procesar.php
@@ -0,0 +1,30 @@
+<?php
+include "bdatos.php";
+
+$user = $_POST ['user'];
+$password = $_POST ['password'];
+$host = $_POST ['host'];
+$busqueda = $_POST ['consulta'];
+str_replace('"', "'", $busqueda);
+
+$datos = new bdatos($host, $user, $password, $busqueda);
+
+$login = $datos -> conectar();
+
+$sentencia = $datos -> consulta($login, $busqueda);
+
+$tabla = $datos -> resultado($sentencia);
+
+?>
+<style type="text/css">
+    tr:nth-child(odd) { background-color:#D2EAE8; }
+
+    tr:nth-of-type(odd) { background-color:#FFFFFF; }
+
+    tr:nth-of-type(even) { background-color:#D2EAE8; }
+
+    .titulo {
+        color: #386639;
+        background-color: #9AEE9B;
+    }
+</style>
