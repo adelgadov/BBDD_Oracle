eb6b4a10368de267158221da95e682f903d34dd8
alvaro <alvaro_delgado_1994@hotmail.com>
2014-11-03 19:09:05 +0100

    commit js

--- js/botones.js
+++ js/botones.js
@@ -8,6 +8,7 @@
         $("#minimizar1").css({display:"none"});
         $("#maximizar1").css({display:"inline"});
 
+
     }
     function maximizar1() {
         $("#capa1").css({display:"inline"});
@@ -17,6 +18,10 @@
 
     function cerrar2() {
         $("#dos").css({display:"none"});
+        $("#posicion option[value='capa2']").remove();
+        capas.unshift("capa2");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar2() {
@@ -33,6 +38,10 @@
 
     function cerrar3() {
         $("#tres").css({display:"none"});
+        $("#posicion option[value='capa3']").remove();
+        capas.unshift("capa3");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar3() {
@@ -49,6 +58,10 @@
 
     function cerrar4() {
         $("#cuatro").css({display:"none"});
+        $("#posicion option[value='capa4']").remove();
+        capas.unshift("capa4");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar4() {
@@ -64,6 +77,10 @@
     }
     function cerrar5() {
         $("#cinco").css({display:"none"});
+        $("#posicion option[value='capa5']").remove();
+        capas.unshift("capa5");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar5() {
@@ -80,6 +97,10 @@
 
     function cerrar6() {
         $("#seis").css({display:"none"});
+        $("#posicion option[value='capa6']").remove();
+        capas.unshift("capa6");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar6() {
@@ -96,6 +117,10 @@
 
     function cerrar7() {
         $("#siete").css({display:"none"});
+        $("#posicion option[value='capa7']").remove();
+        capas.unshift("capa7");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar7() {
@@ -112,6 +137,10 @@
 
     function cerrar8() {
         $("#ocho").css({display:"none"});
+        $("#posicion option[value='capa8']").remove();
+        capas.unshift("capa8");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar8() {
@@ -127,6 +156,10 @@
     }
     function cerrar9() {
         $("#nueve").css({display:"none"});
+        $("#posicion option[value='capa9']").remove();
+        capas.unshift("capa9");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar9() {
@@ -142,6 +175,10 @@
     }
     function cerrar10() {
         $("#diez").css({display:"none"});
+        $("#posicion option[value='capa10']").remove();
+        capas.unshift("capa10");
+        capas.sort(deMenorAMayor);
+        
     }
 
     function minimizar10() {
@@ -155,3 +192,7 @@
         $("#minimizar10").css({display:"inline"});
         $("#maximizar10").css({display:"none"});
     }
+    
+    function deMenorAMayor(elem1, elem2) {return elem1-elem2;}
+
+  
--- js/pasar_datos.js
+++ js/pasar_datos.js
@@ -1,6 +1,8 @@
+
 var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "capa9", "capa10"];
 
-
+$(document).ready()
+{
 
     function enviar_datos() {
         var response;
@@ -8,24 +10,24 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
         var password = document.getElementById('password').value;
         var consulta = document.getElementById('consulta').value;
         var host = document.getElementById('host').value;
-        var posicion=document.getElementById('posicion').options[document.getElementById('posicion').selectedIndex].value;
-        $.ajax({ type: "POST",
-            data: {user:user, password:password, consulta:consulta, host:host},
+        var posicion = document.getElementById('posicion').options[document.getElementById('posicion').selectedIndex].value;
+        $.ajax({
+            type: "POST",
+            data: {user: user, password: password, consulta: consulta, host: host},
             url: "procesar.php #rowspan",
             async: false,
-            success : function(text)
-            {
-                response= text;
+            success: function (text) {
+                response = text;
             }
         });
-        switch (posicion){
+        switch (posicion) {
             case "capa1":
                 $("#capa1").empty();
                 $("#oculto").empty();
 
                 $("#capa1").prepend(response);
                 $("#oculto").empty();
-                $("#uno").css({display:"inline"});
+                $("#uno").css({display: "inline"});
                 break;
             case 'capa2':
                 $("#capa2").empty();
@@ -33,7 +35,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa2").prepend(response);
                 $("#oculto").empty();
-                $("#dos").css({display:"inline"});
+                $("#dos").css({display: "inline"});
 
                 break;
             case 'capa3':
@@ -42,7 +44,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa3").prepend(response);
                 $("#oculto").empty();
-                $("#tres").css({display:"inline"});
+                $("#tres").css({display: "inline"});
                 break;
             case 'capa4':
                 $("#capa4").empty();
@@ -50,7 +52,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa4").prepend(response);
                 $("#oculto").empty();
-                $("#cuatro").css({display:"inline"});
+                $("#cuatro").css({display: "inline"});
                 break;
             case 'capa5':
                 $("#capa5").empty();
@@ -58,7 +60,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa5").prepend(response);
                 $("#oculto").empty();
-                $("#cinco").css({display:"inline"});
+                $("#cinco").css({display: "inline"});
                 break;
             case 'capa6':
                 $("#capa6").empty();
@@ -66,7 +68,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa6").prepend(response);
                 $("#oculto").empty();
-                $("#seis").css({display:"inline"});
+                $("#seis").css({display: "inline"});
                 break;
             case 'capa7':
                 $("#capa7").empty();
@@ -74,7 +76,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa7").prepend(response);
                 $("#oculto").empty();
-                $("#siete").css({display:"inline"});
+                $("#siete").css({display: "inline"});
                 break;
             case 'capa8':
                 $("#capa8").empty();
@@ -82,7 +84,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa8").prepend(response);
                 $("#oculto").empty();
-                $("#ocho").css({display:"inline"});
+                $("#ocho").css({display: "inline"});
                 break;
             case 'capa9':
                 $("#capa9").empty();
@@ -90,7 +92,7 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa9").prepend(response);
                 $("#oculto").empty();
-                $("#nueve").css({display:"inline"});
+                $("#nueve").css({display: "inline"});
                 break;
             case 'capa10':
                 $("#capa10").empty();
@@ -98,67 +100,65 @@ var capas = ["capa2", "capa3", "capa4", "capa5", "capa6", "capa7", "capa8", "cap
 
                 $("#capa10").prepend(response);
                 $("#oculto").empty();
-                $("#diez").css({display:"inline"});
+                $("#diez").css({display: "inline"});
                 break;
         }
 
 
-
     }
 
 
+    var opcion_nombre;
+
+    function myFunction() {
+        switch (capas[0]) {
+            case 'capa2':
+                opcion_nombre = "Segunda capa";
+
+                break;
+            case 'capa3':
+                opcion_nombre = "Tercera capa";
+
+                break;
+            case 'capa4':
+                opcion_nombre = "Cuarta capa";
+
+                break;
+            case 'capa5':
+                opcion_nombre = "Quinta capa";
+
+                break;
+            case 'capa6':
+                opcion_nombre = "Sexta capa";
+
+                break;
+            case 'capa7':
+                opcion_nombre = "Séptima capa";
+
+                break;
+            case 'capa8':
+                opcion_nombre = "Octava capa";
+
+                break;
+            case 'capa9':
+                opcion_nombre = "Novena capa";
+
+                break;
+            case 'capa10':
+                opcion_nombre = "Décima capa";
+
+                break;
+        }
+
+        var x = document.createElement("OPTION");
+        x.setAttribute("value", capas[0]);
+        var t = document.createTextNode(opcion_nombre);
+        x.appendChild(t);
+        document.getElementById("posicion").appendChild(x);
 
 
+        capas.splice(0, 1);
 
-var opcion_nombre;
-function myFunction() {
-    switch (capas[0]){
-        case 'capa2':
-            opcion_nombre = "Segunda capa";
-
-            break;
-        case 'capa3':
-            opcion_nombre = "Tercera capa";
-
-            break;
-        case 'capa4':
-            opcion_nombre = "Cuarta capa";
-
-            break;
-        case 'capa5':
-            opcion_nombre = "Quinta capa";
-
-            break;
-        case 'capa6':
-            opcion_nombre = "Sexta capa";
-
-            break;
-        case 'capa7':
-            opcion_nombre = "Séptima capa";
-
-            break;
-        case 'capa8':
-            opcion_nombre = "Octava capa";
-
-            break;
-        case 'capa9':
-            opcion_nombre = "Novena capa";
-
-            break;
-        case 'capa10':
-            opcion_nombre = "Décima capa";
-
-            break;
     }
-
-    var x = document.createElement("OPTION");
-    x.setAttribute("value", capas[0]);
-    var t = document.createTextNode(opcion_nombre);
-    x.appendChild(t);
-    document.getElementById("posicion").appendChild(x);
-
-
-    capas.splice(0,1);
 
 }
-
